cc756d03e6ca1c52afc845cff03d398af1be8eae
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2020-09-18 16:08:33 +0300

    Update option.php

--- tests/phpunit/tests/option/option.php
+++ tests/phpunit/tests/option/option.php
@@ -104,15 +104,14 @@ class Tests_Option_Option extends WP_UnitTestCase {
 	 */
 	function test_special_option_name_alloption() {
 		$this->expectException( 'WPDieException' );
-
 		delete_option( 'alloptions' );
 	}
 
 	/**
 	 * @ticket 23289
-	 * @expectedException WPDieException
 	 */
 	function test_special_option_name_notoptions() {
+		$this->expectException( 'WPDieException' );
 		delete_option( 'notoptions' );
 	}
 
